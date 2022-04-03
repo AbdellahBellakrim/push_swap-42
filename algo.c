570d9270cb7db5dc72145f2c592f54dfa7741396
Abdellah Bellakrim <abellakr@e2r6p2.1337.ma>
2022-04-03 23:48:31 +0000

    finish smallest numbers

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: abellakr <abellakr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 13:07:00 by abellakr          #+#    #+#             */
-/*   Updated: 2022/04/03 20:04:39 by abellakr         ###   ########.fr       */
+/*   Updated: 2022/04/03 23:38:32 by abellakr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,20 +55,26 @@ void	sort_3(t_args *args)
 //--------------------------------------------- sort 4
 void	sort_4(t_args *args) // have problem in min int
 {
-	int	i;
 	int min;
 
-	i = 0;
 	min = find_min(args);
-	while(i < 4)
-	{
-		if(args->stack_a->content == min)
-		{
+	if(args->stack_a->content == min)
 			pb(&args->stack_a, &args->stack_b);
-			break;
-		}
+	else if(args->stack_a->next->content == min)
+	{
+		swap(&args->stack_a, 'a');
+		pb(&args->stack_a, &args->stack_b);
+	}
+	else if(args->stack_a->next->next->content == min)
+	{
 		bottom_to_top(&args->stack_a, 'a');
-		i++;
+		bottom_to_top(&args->stack_a, 'a');
+		pb(&args->stack_a, &args->stack_b);
+	}
+	else if(args->stack_a->next->next->next->content == min)
+	{
+		bottom_to_top(&args->stack_a, 'a');
+		pb(&args->stack_a, &args->stack_b);
 	}
 	sort_3(args);
 	pa(&args->stack_a, &args->stack_b);
@@ -78,8 +84,10 @@ void	sort_5(t_args *args)
 {
 	int	i;
 	int min;
+	int middle;
 
 	i = 0;
+	middle = args->list_len / 2 + 1;
 	min = find_min(args);
 	while(i < 5)
 	{
@@ -89,7 +97,12 @@ void	sort_5(t_args *args)
 			break;
 		}
 		else
-			top_to_bottom(&args->stack_a, 'a');	
+		{
+			if(i <= middle)
+				top_to_bottom(&args->stack_a, 'a');
+			else if(i > middle)
+				bottom_to_top(&args->stack_a, 'a');
+		}
 		i++;
 	}
 	sort_4(args);
